00a70e11686e745d925e5f758d22a228eabaac80
voyager003 <miraclex000@gmail.com>
2023-11-10 23:36:12 +0900

    feat(StringConverter): add convertToMap() method
    Add new method convertToMap()
    - add util method convert to hashmap
    - test: verify throw exception and map entry

--- src/main/java/christmas/util/StringConverter.java
+++ src/main/java/christmas/util/StringConverter.java
@@ -1,5 +1,7 @@
 package christmas.util;
 
+import java.util.HashMap;
+
 public class StringConverter {
 
     public static int convertToInt(String input) {
@@ -8,5 +10,19 @@ public class StringConverter {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자가 아닌 문자가 포함되었거나 공백이 감지되었습니다.");
         }
+    }
+
+    public static HashMap<String, Integer> convertToMap(String input) {
+        if (!input.matches("^([a-zA-Z가-힣0-9]+-\\d+,)*([a-zA-Z가-힣0-9]+-\\d+)$")) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+        String[] items = input.split(",");
+        HashMap<String, Integer> menu = new HashMap<>();
+
+        for (String item : items) {
+            String[] details = item.split("-");
+            menu.put(details[0], Integer.parseInt(details[1]));
+        }
+        return menu;
     }
 }
--- src/test/java/christmas/util/StringConverterTest.java
+++ src/test/java/christmas/util/StringConverterTest.java
@@ -3,6 +3,8 @@ package christmas.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
@@ -62,5 +64,34 @@ class StringConverterTest {
 
         assertThat(convertedInput).isEqualTo(expectedInput);
         assertThat(convertedInput).isInstanceOf(Integer.class);
+    }
+
+    @Test
+    @DisplayName("문자열을 맵으로 변환하는 기능에서 조건에 맞는 입력이 주어지면 예외를 던지지 않는다.")
+    void convertToMap_doesNotThrow() {
+        /**
+         * given : 조건에 맞는 입력이 주어진다.
+         * when : 문자열을 맵으로 변환하는 기능을 실행한다.
+         * then : 예외를 던지지 않는다.
+         */
+        String input = "해산물파스타-2,레드와인-1";
+
+        assertThatCode(() -> StringConverter.convertToMap(input))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("문자열을 맵으로 변환하는 기능 수행 후, 객체의 상태와 값을 확인한다.")
+    void convertToMap_validateMap() {
+        /**
+         * given : 조건에 맞는 입력이 주어진다.
+         * when : 문자열을 맵으로 변환하는 기능을 실행한다.
+         * then :
+         */
+        String input = "해산물파스타-2,레드와인-1";
+        HashMap<String, Integer> menu = StringConverter.convertToMap(input);
+
+        assertThat(menu).containsEntry("해산물파스타", 2)
+                        .containsEntry("레드와인", 1);
     }
 }
