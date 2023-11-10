a444c92703b569e9bb308c8278c563439380ed1d
voyager003 <miraclex000@gmail.com>
2023-11-10 19:32:51 +0900

    feat(Week): add enum Week
    Add enum to represent day of week

---
+++ src/main/java/christmas/domain/date/Week.java
@@ -0,0 +1,24 @@
+package christmas.domain.date;
+
+public enum Week {
+    SUNDAY("일", 3),
+    MONDAY("월", 4),
+    TUESDAY("화", 5),
+    WEDNESDAY("수", 6),
+    THURSDAY("목", 7),
+    FRIDAY("금", 1),
+    SATURDAY("토", 2);
+
+    private final String day;
+    private final int index;
+
+    Week(String day, int index) {
+        this.day = day;
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+}
+
