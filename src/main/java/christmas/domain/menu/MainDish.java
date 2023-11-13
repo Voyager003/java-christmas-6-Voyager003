7f0d7e84726f4ba339d94e9b6ceb18339757d07b
voyager003 <miraclex000@gmail.com>
2023-11-14 00:16:07 +0900

    fix(MainDish): fix enum name
    modify enum name due to incorrect menu name entry

--- src/main/java/christmas/domain/menu/MainDish.java
+++ src/main/java/christmas/domain/menu/MainDish.java
@@ -3,7 +3,7 @@ package christmas.domain.menu;
 public enum MainDish implements Menu {
 
     TBONE_STEAK("티본스테이크", 55_000),
-    BARBECUE_RIBS("바베큐립", 54_000),
+    BARBECUE_RIBS("바비큐립", 54_000),
     SEAFOOD_PASTA("해산물파스타", 35_000),
     CHRISTMAS_PASTA("크리스마스파스타", 25_000);
 
