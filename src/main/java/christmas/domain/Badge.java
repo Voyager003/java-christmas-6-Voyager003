83d3882c85b49124ae564772882557b8d4b8f881
voyager003 <miraclex000@gmail.com>
2023-11-13 22:23:58 +0900

    refactor(Badge): add new field badge and constant NONE
    - badge: name of badge
    - add NONE to represent none case

--- src/main/java/christmas/domain/Badge.java
+++ src/main/java/christmas/domain/Badge.java
@@ -1,13 +1,20 @@
 package christmas.domain;
 
 public enum Badge {
-    STAR(5_000),
-    TREE(10_000),
-    SANTA(20_000);
+    STAR("별", 5_000),
+    TREE("트리", 10_000),
+    SANTA("산타", 20_000),
+    NONE("없음", 0);
 
+    private String badge;
     private int price;
 
-    Badge(int price) {
+    Badge(String badge, int price) {
+        this.badge = badge;
         this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
