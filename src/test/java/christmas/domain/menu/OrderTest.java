cfd44f4e224cfea3c9a0c5f457b840b59d028fb7
voyager003 <miraclex000@gmail.com>
2023-11-14 15:08:44 +0900

    test(OrderTest): fix OrderTest
    Fix that construct Order class directly update test code due to menu input changes

--- src/test/java/christmas/domain/menu/OrderTest.java
+++ src/test/java/christmas/domain/menu/OrderTest.java
@@ -1,10 +1,13 @@
 package christmas.domain.menu;
 
+import christmas.dao.MenuRepository;
+import christmas.domain.Order;
 import christmas.domain.SelectionMenu;
+import christmas.domain.date.VisitDate;
+import christmas.service.MenuService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Map;
 
 import static christmas.domain.menu.Beverage.RED_WINE;
 import static christmas.domain.menu.MainDish.SEAFOOD_PASTA;
@@ -13,27 +16,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class OrderTest {
 
+    private MenuService menuService;
+    private MenuRepository menuRepository;
+
+    @BeforeEach
+    public void setup() {
+        menuRepository = MenuRepository.getInstance();
+        menuService = new MenuService(menuRepository);
+
+        // 메뉴에 샴페인과 레드와인을 추가한다.
+        menuRepository.saveMenu(SEAFOOD_PASTA);
+        menuRepository.saveMenu(RED_WINE);
+    }
+
     @Test
     @DisplayName("주문 금액을 계산하는 기능을 테스트한다.")
     void validateMenu() {
         /**
-         * given : 레드와인과 해산물파스타를 주문하고, 초기 주문 금액은 0원이다.
-         * when : 레드와인 2잔, 해산물파스타 1개를 주문한다.
-         * then : 총 주문 금액은 (60000원 * 2) + (30000원 * 1)으로 155000원이다.
+         * given : 레드와인과 해산물파스타를 주문한다.
+         * when : 주문 정보를 생성한다.
+         * then : 총 주문 금액은 (60000원 * 2) + (35000원 * 1)으로 155000원이다.
          */
-        SelectionMenu selectionMenu = new SelectionMenu();
-        Menu wine = RED_WINE;
-        Menu pasta = SEAFOOD_PASTA;
-        int orderAmount = 0;
+        VisitDate visitDate = new VisitDate("1");
+        SelectionMenu selectionMenu = new SelectionMenu("레드와인-2,해산물파스타-1");
 
-        selectionMenu.saveMenu(wine, 2);
-        selectionMenu.saveMenu(pasta, 1);
-        Map<Menu, Integer> menus = selectionMenu.getSelectionMenu();
-        for (Map.Entry<Menu, Integer> entry : menus.entrySet()) {
-            Menu menu = entry.getKey();
-            int quantity = entry.getValue();
-            orderAmount += menu.getPrice() * quantity;
-        }
+        Order order = new Order(visitDate, selectionMenu);
+        int orderAmount = order.getOrderAmount();
 
         assertThat(orderAmount).isEqualTo(155_000);
     }
