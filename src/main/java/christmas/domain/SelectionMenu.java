b5e72030b3e36f9cd9255d7a2392cf360e341afb
voyager003 <miraclex000@gmail.com>
2023-11-14 13:47:42 +0900

    refactor(SelectionMenu): move business logic from service to domain and
    Add SelectionMenu generateSelectionMenu() and test code
    - move business logic save the selection menu from SelectionMenuService to SelectionMenu domain
    - it ensure that domain itself contains the rules and behaviors related to it, which improves code maintainability and readability.
    - test: verify method work

--- src/main/java/christmas/domain/SelectionMenu.java
+++ src/main/java/christmas/domain/SelectionMenu.java
@@ -1,20 +1,30 @@
 package christmas.domain;
 
 
+import christmas.dao.MenuRepository;
 import christmas.domain.menu.Menu;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class SelectionMenu {
-
+    private final MenuRepository menuRepository = MenuRepository.getInstance();
     private final Map<Menu, Integer> selectionMenu = new HashMap<>();
 
-    public void saveMenu(Menu menuName, int quantity) {
-        selectionMenu.put(menuName, quantity);
+    public SelectionMenu(HashMap<String, Integer> input) {
+        generateSelectionMenu(input);
     }
 
     public Map<Menu, Integer> getSelectionMenu() {
         return selectionMenu;
+    }
+
+    private void generateSelectionMenu(HashMap<String, Integer> input) {
+        for (Map.Entry<String, Integer> entry : input.entrySet()) {
+            String menuName = entry.getKey();
+            int quantity = entry.getValue();
+            Menu menu = menuRepository.findByName(menuName);
+            selectionMenu.put(menu, quantity);
+        }
     }
 }
--- src/test/java/christmas/domain/SelectionMenuTest.java
+++ src/test/java/christmas/domain/SelectionMenuTest.java
@@ -1,10 +1,8 @@
-package christmas.service;
+package christmas.domain;
 
 import christmas.dao.MenuRepository;
-import christmas.domain.SelectionMenu;
-import christmas.domain.menu.Beverage;
-
 import christmas.domain.menu.Menu;
+import christmas.service.MenuService;
 import christmas.util.StringConverter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,42 +13,39 @@ import java.util.Map;
 
 import static christmas.domain.menu.Beverage.CHAMPAGNE;
 import static christmas.domain.menu.Beverage.RED_WINE;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-class SelectionMenuServiceTest {
+
+class SelectionMenuTest {
 
     private MenuService menuService;
     private MenuRepository menuRepository;
-    private SelectionMenuService selectionMenuService;
-
 
     @BeforeEach
     public void setup() {
         menuRepository = MenuRepository.getInstance();
         menuService = new MenuService(menuRepository);
-        selectionMenuService = new SelectionMenuService();
 
         // 메뉴에 샴페인과 레드와인을 추가한다.
-        menuRepository.saveMenu(Beverage.CHAMPAGNE);
+        menuRepository.saveMenu(CHAMPAGNE);
         menuRepository.saveMenu(RED_WINE);
     }
 
     @Test
-    @DisplayName("메뉴를 선택하고 데이터가 담겨있는지 확인한다.")
-    public void saveSelectionMenu_successful() {
+    @DisplayName("입력한 메뉴가 선택 메뉴에 저장되는지 확인한다.")
+    void generateSelectionMenu_expectMenu() {
         /**
-         * given : 입력값으로 샴페인-1,레드와인-1이 주어진다.
-         * when : 입력값을 저장한다.
-         * then : 예상한 메뉴가 저장되어 있는지 확인한다.
+         * given : 샴페인과 레드와인을 입력한다.
+         * when : 입력한 메뉴를 선택 메뉴에 저장한다.
+         * then : 선택 메뉴에는 샴페인 1개, 레드와인 1개가 저장되어 있다.
          */
         String input = "샴페인-1,레드와인-1";
-        HashMap<String, Integer> inputMap = StringConverter.convertToMap(input);
+        HashMap<String, Integer> inputs = StringConverter.convertToMap(input);
 
-        SelectionMenu menu = selectionMenuService.saveSelectionMenu(inputMap);
+        SelectionMenu menu = new SelectionMenu(inputs);
         Map<Menu, Integer> selectionMenu = menu.getSelectionMenu();
 
-
         assertThat(selectionMenu).containsEntry(CHAMPAGNE, 1)
-                                .containsEntry(RED_WINE, 1);
+                .containsEntry(RED_WINE, 1);
     }
 }
