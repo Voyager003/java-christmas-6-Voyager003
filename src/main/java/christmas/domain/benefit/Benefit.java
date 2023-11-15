ee99b7d99570b1fa2eb8c4971afc34de5d0d47da
voyager003 <miraclex000@gmail.com>
2023-11-15 21:45:29 +0900

    refactor(Benefit): fix Benefit class
    Fix Benefit class for maintainable
    - accept list DiscountPolicy object in constructor enable Benefit class to apply any number of discount policies, enhancing maintainability
    - calculateBenefitAmount method was replaced by applyDiscountPolicies method, which apply each discount policy in provided list to order
    - getDiscount method were removed, as new design handle this functionality with applyDiscountPolicies() method
    - add applyGiftEvent() method encapsulates logic for granting gift, improving readability and maintainability
    - add getTotalBenefitAmount() method now calculates sum from the BenefitDetail objects in benefits list, instead of summing values in a map
    - add getGiftEventAmount method() has been added to get gift event amount from benefits list

--- src/main/java/christmas/domain/benefit/Benefit.java
+++ src/main/java/christmas/domain/benefit/Benefit.java
@@ -1,41 +1,37 @@
 package christmas.domain.benefit;
 
-
-import christmas.domain.discount.*;
+import christmas.domain.discount.DiscountPolicy;
 import christmas.domain.order.Order;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
-import static christmas.domain.menu.Beverage.CHAMPAGNE;
-import static java.util.Comparator.comparing;
+import java.util.Comparator;
+import java.util.List;
 
 public class Benefit {
-    private static final int GIFT_EVENT_AMOUNT = 120_000;
-    private Map<String, Integer> benefits = new HashMap<>();
+    private List<BenefitDetail> benefits = new ArrayList<>();
+    private Gift gift;
     private Badge badge;
 
-    public Benefit(Order order) {
-        calculateBenefitAmount(order);
+    public Benefit(Order order, List<DiscountPolicy> policies) {
+        applyDiscountPolicies(order, policies);
+        applyGiftEvent(order);
         badge = grantBadge();
-    }
-
-    public int getTotalBenefitAmount() {
-        return benefits.values().stream()
-                .mapToInt(Integer::intValue)
-                .sum();
     }
 
     public int calculateTotalDiscount(Order order) {
         if (order.getOrderAmount() < 10_000) {
             return order.getOrderAmount();
         }
-        int totalDiscountAmount = getTotalBenefitAmount() - benefits.get("증정 이벤트");
+        int totalDiscountAmount = getTotalBenefitAmount() - getGiftEventAmount();
         return order.getOrderAmount() - totalDiscountAmount;
     }
 
-    public Map<String, Integer> getBenefits() {
+    public int getTotalBenefitAmount() {
+        return benefits.stream().mapToInt(BenefitDetail::getAmount).sum();
+    }
+
+    public List<BenefitDetail> getBenefits() {
         return benefits;
     }
 
@@ -43,48 +39,42 @@ public class Benefit {
         return badge;
     }
 
-    private void calculateBenefitAmount(Order order) {
-        benefits.put("크리스마스 디데이 할인", getChristmasDiscount(order));
-        benefits.put("평일 할인", getWeekdayDiscount(order));
-        benefits.put("주말 할인", getWeekendDiscount(order));
-        benefits.put("특별 할인", getSpecialDiscount(order));
-        grantGiftEvent(order);
+    public String getGift() {
+        return gift.getGiftMenuName();
     }
 
-    private void grantGiftEvent(Order order) {
-        int eventBenefit = 0;
-        if (order.getOrderAmount() >= GIFT_EVENT_AMOUNT) {
-            eventBenefit = CHAMPAGNE.getPrice();
+    private void applyDiscountPolicies(Order order, List<DiscountPolicy> policies) {
+        policies.forEach(policy -> {
+            BenefitDetail benefitDetail = policy.applyDiscount(order);
+            if (benefitDetail.isNonZero()) {
+                benefits.add(benefitDetail);
+            }
+        });
+    }
+
+    private int getGiftEventAmount() {
+        return benefits.stream()
+                .filter(benefit -> benefit.getName().equals("증정 이벤트"))
+                .mapToInt(BenefitDetail::getAmount)
+                .findFirst()
+                .orElse(0);
+    }
+
+    private void applyGiftEvent(Order order) {
+        gift = new Gift();
+        gift.grant(order);
+        BenefitDetail giftBenefitDetail = gift.getBenefitDetail();
+        if (giftBenefitDetail.isNonZero()) {
+            benefits.add(giftBenefitDetail);
         }
-        benefits.put("증정 이벤트", eventBenefit);
     }
 
     private Badge grantBadge() {
         int totalBenefitAmount = getTotalBenefitAmount();
         return Arrays.stream(Badge.values())
-                .sorted(comparing(Badge::getPrice).reversed())
+                .sorted(Comparator.comparing(Badge::getPrice).reversed())
                 .filter(badge -> badge.isPriceGreaterOrEqual(totalBenefitAmount))
                 .findFirst()
                 .orElse(Badge.NONE);
-    }
-
-    private int getChristmasDiscount(Order order) {
-        DiscountPolicy christmasDiscountPolicy = new ChristmasDiscountPolicy();
-        return christmasDiscountPolicy.discount(order);
-    }
-
-    private int getWeekdayDiscount(Order order) {
-        DiscountPolicy weekdayDiscountPolicy = new WeekdayDiscountPolicy();
-        return weekdayDiscountPolicy.discount(order);
-    }
-
-    private int getWeekendDiscount(Order order) {
-        DiscountPolicy weekendDiscountPolicy = new WeekendDiscountPolicy();
-        return weekendDiscountPolicy.discount(order);
-    }
-
-    private int getSpecialDiscount(Order order) {
-        DiscountPolicy specialDiscountPolicy = new SpecialDiscountPolicy();
-        return specialDiscountPolicy.discount(order);
     }
 }
