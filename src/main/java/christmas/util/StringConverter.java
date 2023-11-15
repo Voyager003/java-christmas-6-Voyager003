8b76d067363d9b6f74fd7543ebd12a5f3e40f5ce
voyager003 <miraclex000@gmail.com>
2023-11-15 16:05:32 +0900

    refactor(StringConverter): fix convertToMap() util method
    - fix to only accept string pattern that starts with korean characters and ends with numbers, separated by hyphen
    - additional extracted method 'validateInput'

--- src/main/java/christmas/util/StringConverter.java
+++ src/main/java/christmas/util/StringConverter.java
@@ -13,9 +13,7 @@ public class StringConverter {
     }
 
     public static HashMap<String, Integer> convertToMap(String input) {
-        if (!input.matches("^([a-zA-Z가-힣0-9]+-\\d+,)*([a-zA-Z가-힣0-9]+-\\d+)$")) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-        }
+        validateInput(input);
         String[] items = input.split(",");
         HashMap<String, Integer> menu = new HashMap<>();
 
@@ -24,5 +22,11 @@ public class StringConverter {
             menu.put(details[0], Integer.parseInt(details[1]));
         }
         return menu;
+    }
+
+    private static void validateInput(String input) {
+        if (!input.matches("^([가-힣]+-\\d+,)*([가-힣]+-\\d+)$")) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
     }
 }
