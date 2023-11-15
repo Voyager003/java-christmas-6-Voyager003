fe4947fec3f8653d5a0d89786e25b2355961e99e
voyager003 <miraclex000@gmail.com>
2023-11-15 15:25:24 +0900

    refactor(VisitDateTest): refactor test code about repetitive task
    - previously, each test case had separate methods for different input values
    - now uses parameterized tests, allowing same test to be run multiple times with different input values
    - effect code duplication, improves readability

--- src/test/java/christmas/domain/date/VisitDateTest.java
+++ src/test/java/christmas/domain/date/VisitDateTest.java
@@ -2,6 +2,8 @@ package christmas.domain.date;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
@@ -9,30 +11,28 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 
 class VisitDateTest {
 
-    @Test
+    @ValueSource(strings = {"1", "2", "3", "4", "5"})
     @DisplayName("올바른 조건의 입력이 주어진다면, 예외를 던지지 않는다.")
-    void validateVisitDate_doesNotThrow() {
+    @ParameterizedTest
+    void validateVisitDate_doesNotThrow(String input) {
         /**
-         * given : 올바른 조건인 1이 주어진다.
+         * given : 올바른 조건인 1, 2, 3, 4, 5가 주어진다.
          * when : VisitDate 인스턴스를 생성한다.
          * then : 예외를 발생시키지 않는다.
          */
-        String input = "1";
-
         assertThatCode(() -> new VisitDate(input))
                 .doesNotThrowAnyException();
     }
 
-    @Test
+    @ValueSource(strings = {"0", "32", "36"})
     @DisplayName("유효하지 않은 조건의 입력이 주어진다면, 예외를 던진다.")
-    void validateVisitDate_throwException() {
+    @ParameterizedTest
+    void validateVisitDate_throwException(String input) {
         /**
-         * given : 입력 조건(1~31)을 벗어난 수 36이 주어진다.
+         * given : 입력 조건(1~31사이의 정수)을 벗어난 수 0, 32, 36이 주어진다.
          * when : VisitDate 인스턴스를 생성한다.
          * then : IllegalArgumentException을 발생시킨다.
          */
-        String input = "36";
-
         assertThatCode(() -> new VisitDate(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
