d3b2a8c0c992de2fef962f2a4cd31144b8f2ef61
voyager003 <miraclex000@gmail.com>
2023-11-15 19:23:07 +0900

    refactor(OutputView): fix using constant
    add constant for OutputView message

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -10,17 +10,27 @@ import java.util.Map;
 public class OutputView {
 
     private static final String MESSAGE_NOTHING = "없음";
+    private static final String MESSAGE_INTRODUCE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String MESSAGE_INPUT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String MESSAGE_INPUT_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    private static final String MESSAGE_MENU_HEADER = "\n<주문 메뉴>";
+    private static final String MESSAGE_BEFORE_DISCOUNT = "\n<할인 전 총주문 금액>";
+    private static final String MESSAGE_GIFT = "\n<증정 메뉴>";
+    private static final String MESSAGE_BENEFIT_DETAIL = "\n<혜택 내역>";
+    private static final String MESSAGE_TOTAL_DETAIL = "\n<총혜택 금액>";
+    private static final String MESSAGE_AFTER_DISCOUNT = "\n<할인 후 예상 결제 금액>";
+    private static final String MESSAGE_EVENT_BADGE = "\n<12월 이벤트 배지>";
 
     public static void printMessageIntroduce() {
-        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println(MESSAGE_INTRODUCE);
     }
 
     public static void printMessageInputDate() {
-        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        System.out.println(MESSAGE_INPUT_DATE);
     }
 
     public static void printMessageInputMenu() {
-        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        System.out.println(MESSAGE_INPUT_MENU);
     }
 
     public static void printMessageEvent(Order order) {
@@ -29,7 +39,7 @@ public class OutputView {
     }
 
     public static void printMenuHeader() {
-        System.out.println("\n<주문 메뉴>");
+        System.out.println(MESSAGE_MENU_HEADER);
     }
 
     public static void printMenu(String menuName, int count) {
@@ -37,12 +47,12 @@ public class OutputView {
     }
 
     public static void printBeforeDiscount(int orderAmount) {
-        System.out.println("\n<할인 전 총주문 금액>");
+        System.out.println(MESSAGE_BEFORE_DISCOUNT);
         System.out.println(NumberFormat.getInstance().format(orderAmount) + "원");
     }
 
     public static void printGift(Benefit benefit) {
-        System.out.println("\n<증정 메뉴>");
+        System.out.println(MESSAGE_GIFT);
         Map<String, Integer> benefits = benefit.getBenefits();
         String giftMenu = MESSAGE_NOTHING;
         if (benefits.get("증정 이벤트") > 0) {
@@ -52,7 +62,7 @@ public class OutputView {
     }
 
     public static void printBenefitDetail(Benefit benefit) {
-        System.out.println("\n<혜택 내역>");
+        System.out.println(MESSAGE_BENEFIT_DETAIL);
         Map<String, Integer> benefits = benefit.getBenefits();
         boolean hasBenefit = false;
         for (Map.Entry<String, Integer> entry : benefits.entrySet()) {
@@ -68,7 +78,7 @@ public class OutputView {
     }
 
     public static void printTotalDetail(Benefit benefit) {
-        System.out.println("\n<총혜택 금액>");
+        System.out.println(MESSAGE_TOTAL_DETAIL);
         int amount = benefit.getTotalBenefitAmount();
         String formattedAmount = NumberFormat.getInstance().format(amount);
         if (amount != 0) {
@@ -78,12 +88,12 @@ public class OutputView {
     }
 
     public static void printAfterDiscount(int totalDiscountAmount) {
-        System.out.println("\n<할인 후 예상 결제 금액>");
+        System.out.println(MESSAGE_AFTER_DISCOUNT);
         System.out.println(NumberFormat.getInstance().format(totalDiscountAmount) + "원");
     }
 
     public static void printEventBadge(Benefit benefit) {
-        System.out.println("\n<12월 이벤트 배지>");
+        System.out.println(MESSAGE_EVENT_BADGE);
         Badge badge = benefit.getBadge();
         System.out.println(badge.toString());
     }
